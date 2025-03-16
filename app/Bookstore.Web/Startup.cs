ff9b105ac4249904e4879281178965706fd86f9f
NoReply Amazon <no-reply@amazon.com>
2025-03-16 02:19:16 +0000

    Revert "[Commit] Iteration 21 #errors = 5 <= 4."
    This reverts commit cee1433b17430c8b0e58bbe68896d5b010474c00.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Bookstore.Web.Setup; // Assuming LoggingSetup is in this namespace
 
 namespace Bookstore.Web
 {
